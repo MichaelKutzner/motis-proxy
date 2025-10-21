9babbdfe8c7eb79889c85dc1a64b98e3cac44569
Michael Kutzner <174690291+MichaelKutzner@users.noreply.github.com>
2025-10-21 20:38:12 +0200

    Fix error handling

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use hyper::{
     Request, Response, body::Incoming, client::conn::http1::SendRequest, server::conn::http1,
 };
 use hyper_util::rt::TokioIo;
-use std::{convert::Infallible, net::SocketAddr, sync::Arc};
+use std::{net::SocketAddr, sync::Arc};
 use tokio::net::{TcpListener, TcpStream};
 use tower::ServiceBuilder;
 
@@ -13,24 +13,18 @@ mod time;
 async fn forward_request(
     req: Request<Incoming>,
     backend_server: &String,
-) -> Result<Response<Incoming>, Infallible> {
+) -> Result<Response<Incoming>, ErrorBox> {
     // println!("Sending request to '{}'", backend_server);
     Ok(connect_to_backend(backend_server)
-        .await
-        .expect("Connection to backend server failed")
+        .await?
         .send_request(req)
-        .await
-        .expect("Request failed"))
+        .await?)
 }
 
-async fn connect_to_backend(backend_server: &String) -> Result<SendRequest<Incoming>, Infallible> {
-    let stream = TcpStream::connect(backend_server)
-        .await
-        .expect("Failed to connect to the backend server");
+async fn connect_to_backend(backend_server: &String) -> Result<SendRequest<Incoming>, ErrorBox> {
+    let stream = TcpStream::connect(backend_server).await?;
     let io = TokioIo::new(stream);
-    let (sender, conn) = hyper::client::conn::http1::handshake(io)
-        .await
-        .expect("Handshake failed");
+    let (sender, conn) = hyper::client::conn::http1::handshake(io).await?;
     tokio::task::spawn(async move {
         if let Err(err) = conn.await {
             println!("Connection failed: {:?}", err);
@@ -43,7 +37,7 @@ async fn connect_to_backend(backend_server: &String) -> Result<SendRequest<Incom
 async fn proxy(
     req: Request<Incoming>,
     config: Arc<config::Config>,
-) -> Result<Response<Incoming>, Infallible> {
+) -> Result<Response<Incoming>, ErrorBox> {
     // println!("Request starts in {:?} days", current_day_offset);
     if let Some(current_day_offset) = time::get_current_day_offset(&req) {
         for backend in &config.backends {
@@ -56,7 +50,7 @@ async fn proxy(
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+async fn main() -> Result<(), ErrorBox> {
     let config = config::Config::load();
     println!("Used config: {:?}", config);
     let addr = SocketAddr::from(config.bind_addr);
@@ -76,3 +70,5 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         });
     }
 }
+
+type ErrorBox = Box<dyn std::error::Error + Send + Sync>;
